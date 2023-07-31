3b27409e843ea32afb896347f94b46d91b89f176
Raushan Kumar <109777730+yaduanshiraushan@users.noreply.github.com>
2023-07-31 07:54:50 +0530

    Add files via upload
    login Activity and Register activity by using firebase as a database and kotlin as a language

---
+++ musicplayer/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.internshala.musicplayer
+
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.FrameLayout
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var coordinator: CoordinatorLayout
+    lateinit var toolbar: Toolbar
+    lateinit var frameLayout: FrameLayout
+    lateinit var navigationView: NavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById(R.id.DrawerLayout)
+        coordinator = findViewById(R.id.CoordinatorLayout)
+        toolbar = findViewById(R.id.Toolbar)
+        frameLayout = findViewById(R.id.FrameLayout)
+        navigationView = findViewById(R.id.NavigationView)
+        setUpToolbar()
+
+        val actionBarDrawerToggle = ActionBarDrawerToggle(
+            this@MainActivity,drawerLayout,
+            R.string.open_drawer,
+            R.string.close_drawer
+        )
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+    }
+
+    fun setUpToolbar() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "Music Player"
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        val id = item.itemId
+
+        if(id == android.R.id.home){
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+
+
+
+}
---
+++ musicplayer/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.internshala.musicplayer
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val startAct = Intent(this@SplashActivity,RegistrationActivity::class.java)
+            startActivity(startAct)
+        },1000)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        supportActionBar?.hide()
+    }
+
+}
